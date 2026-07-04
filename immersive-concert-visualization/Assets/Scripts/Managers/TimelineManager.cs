a71de85cc678191315cc411c0c3d9db8ca1f58b7
Siya Sonpatki <siyasonpatki2@gmail.com>
2026-07-04 12:15:01 -0700

    create the timeline manager

---
+++ immersive-concert-visualization/Assets/Scripts/Managers/TimelineManager.cs
@@ -0,0 +1,27 @@
+// responsibilities:
+    // ask audiomanager for curr song time
+    // store list of event
+    // trigger each event once corresponding timestamp reached
+
+using UnityEngine;  // import unity core library
+using System.Collections.Generic;   // since using List<>
+
+// class TimelineManager inheriting from MonoBehavior (i.e., can be attached to gameobject)
+public class TimelineManager : MonoBehaviour
+{
+    private List<TimelineEvent> timelineEvents = new List<TimelineEvent>();  // store list of events
+
+    private void Update()
+    {
+        float currentTime = AudioManager.Instance.CurrentTime;  // ask audiomanager for curr song time
+        
+        for (int i = 0; i < timelineEvents.Count; i++)
+        {
+            if (timelineEvents[i].timestamp <= currentTime && !timelineEvents[i].hasExecuted)
+            {
+                timelineEvents[i].Execute();  // trigger each event once corresponding timestamp reached
+                timelineEvents[i].hasExecuted = true;
+            }
+        }
+    }
+}
